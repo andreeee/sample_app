8e0018ce0853a8d6d062e9d1c56a473c74923edf
Andre Williams <andrewilliamsyo@gmail.com>
2013-03-15 10:27:28 -0700

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Andre Williams"
+    email "andrewilliamsyo@gmail.com"
+    password "foobar12"
+    password_confirmation "foobar12"
+  end
+end
